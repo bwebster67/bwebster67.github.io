c27b8c6103d828ef82fb33342023071d228430ad
Ben Webster <143748162+bwebster67@users.noreply.github.com>
2025-02-03 12:18:28 -0800

    Removed light/dark mode button

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// Dark Mode Toggle
-const darkModeToggle = document.getElementById('dark-mode-toggle');
 
-darkModeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    const isDarkMode = document.body.classList.contains('dark-mode');
-    localStorage.setItem('darkMode', isDarkMode);
-});
-
-// Load Dark Mode Preference
-window.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('darkMode') === 'true') {
-        document.body.classList.add('dark-mode');
-    }
-});
