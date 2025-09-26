8e01c38d1fc7ed23cbb8ab4bf889396a2552c5fc
Ben Webster <bkwebster@willamette.edu>
2025-09-26 16:47:32 -0700

    Functioning projects section!

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 // 1. Select the element by its ID
 const mySpan = document.getElementById('header');
 
+//
+const projectCard = document.getElementById('universal-project-card');
+
 // 2. Get the current text content
 const currentText = mySpan.innerText;
 var originalText = currentText;
@@ -49,46 +52,77 @@ function alterOneCharacter(originalInputText, currentInputText) {
         : replacedChar;
     let stringCombined = stringStart + replacingChar + stringEnd;
 
-    console.log(rand);
-    console.log("replaced: "+ replacedChar + " with " + replacingChar + " at index " + rand);
+    // console.log(rand);
+    // console.log("replaced: "+ replacedChar + " with " + replacingChar + " at index " + rand);
     return stringCombined;
 }
 
 function updateText() {
-    // if (bool) {
-    //     mySpan.innerText = "TEST";
-    //     console.log("updating to TEST");
-    // }
-    // else {
-    //     mySpan.innerText = originalText;
-    //     console.log("updating to 'originalText'");
-    // }
-    // bool = !bool;
-
-
-    // let rand = randomIntInclusive(0, originalText.length);
-
-    // Computes both halves of the string and combines them
-    // if (rand != originalText.length) {
-    //     stringStart = mySpan.innerText.slice(0, rand)
-    //     stringEnd = mySpan.innerText.slice(rand + 1, originalText.length)
-    // } else {
-    //     stringStart = mySpan.innerText.slice(0, rand-1)
-    //     stringEnd = ""
-    // }
-    // replacedChar = mySpan.innerText[rand];
-    // replacingChar = alternateLettersDict[replacedChar][0];
-    // stringCombined = stringStart + replacingChar + stringEnd
-
-    // console.log(stringCombined)
-    // console.log("replaced: "+ replacedChar + " with " + replacingChar + " at index " + rand);
-    // mySpan.innerText = stringCombined;
     alteredText = alterOneCharacter(originalText, currentText);
     alteredText = alterOneCharacter(alteredText, currentText);
-    console.log(alteredText);
+    // console.log(alteredText);
     mySpan.innerText = alteredText;
 
 }
 
-// Makes this run once per X miliseconds 
+// Makes glitch text run once per X miliseconds 
 setInterval(updateText, 750);
+
+
+
+
+
+
+
+let projectIDs = {
+    0 : 'pedalogical',
+    1 : 'gjk',
+    2 : 'sha',
+    3 : 'nav',
+    4 : 'msb',
+}
+
+function loadProjectCard(index) {
+    // console.log("BLAAAAAAA");
+    console.log(index);
+}
+
+
+const projectLabels = document.querySelectorAll('.project-label');
+console.log(projectLabels)
+const projectContents = document.querySelectorAll('.project-content');
+let currentlySelectedLabel = projectLabels[0]; 
+let currentlySelectedIndex = 0;
+currentlySelectedLabel.classList.add('selected');
+const targetContent = document.getElementById(projectLabels[currentlySelectedIndex].getAttribute('data-project')); 
+if (targetContent) {
+    targetContent.classList.add('active');
+}
+loadProjectCard(currentlySelectedIndex);
+
+// Defines the function to happen on click
+projectLabels.forEach((label, index) => {
+            label.addEventListener('click', () => {
+
+                const targetId = label.getAttribute('data-project');
+                const targetContent = document.getElementById(targetId); 
+
+                // First, remove the 'selected' class from all labels
+                projectLabels.forEach(l => l.classList.remove('selected'));
+                projectContents.forEach(c => c.classList.remove('active'));
+
+                // Then, add the 'selected' class to the one that was just clicked
+                label.classList.add('selected');
+                
+                // And add 'active' to the corresponding content to show it
+                if (targetContent) {
+                    targetContent.classList.add('active');
+                }
+
+                // Update our variable and log the selected item's text to the console
+                currentlySelectedLabel = label;
+                currentlySelectedIndex = index;
+                console.log('Currently selected:', currentlySelectedLabel.textContent, ' at ' + index);
+                // loadProjectCard(currentlySelectedIndex);
+            });
+        });
